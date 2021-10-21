25e25920fac033760635761afba15597cbdf8649
tharanesh21 <79951762+tharanesh21@users.noreply.github.com>
2021-10-21 10:42:28 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+echo "Starting to deploy docker image.."
+DOCKER_IMAGE=tharanesh21/seconddose
+docker pull $DOCKER_IMAGE:latest
+docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
+docker run -d -p 8080:8080 $DOCKER_IMAGE
